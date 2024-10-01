1d7187ddeff35b2991a755154f2e1edcf63c3577
Blackt <155835815+bla999ckt@users.noreply.github.com>
2024-10-01 23:28:19 +0800

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -23,8 +23,12 @@ def main():
     events = get_events()
 
 
-    for event in events:
-        print(event.name)
+    for event in events: 
+        print(f"{event.name} : {str(event.start_time)[0:2]}:{str(event.start_time)[2:4]} - {str(event.end_time)[0:2]}:{str(event.end_time)[2:4]} ") 
+        # There's a bug here when outputing the times like 06:50 cuz when i converted them to integers to make it easier for calculations
+        # when they are converted back to strings they lose the  initial 0 and therefore the bugg accures in my slicing technique
+        # but i won't fix it cuz this like is just to see if my events list is accurate and i don't really need to print the times...
+        # aka this is just a debugging temporary line 
 
 
 def get_events():
@@ -35,11 +39,30 @@ def get_events():
         if name == ";":
             break
         else:
-            starting = input("Enter starting time (foramt : HH:MM in 24h system ex: 16:59): ")
-            ending = input("Enter ending time (foramt : HH:MM in 24h system ex: 16:59):   ")
+            while True:
+                starting = input("Enter starting time (foramt : HH:MM in 24h system ex: 16:59): ")
+                if(len(starting) != 5 or int(starting[0:2]) < 0 or int(starting[0:2]) > 23  or int(starting[3:5]) < 0 or int(starting[3:5]) > 59):
+                    print("WRONG INPUT")
+                    continue
+                else:
+                    start = int(starting[0:2] + starting[3:5])
+                    break
+                
+            while True:
+                ending = input("Enter ending time (foramt : HH:MM in 24h system ex: 16:59):   ")
+                if(len(ending) != 5 or int(ending[0:2]) < 0 or int(ending[0:2]) > 23  or int(ending[3:5]) < 0 or int(ending[3:5]) > 59):
+                    print("WRONG INPUT")
+                    continue
 
-            events_list.append(event(name, starting, ending))
+                end = int(ending[0:2] + ending[3:5])
+                if end < start:
+                    print("WRONG INPUT")
+                    continue
+                
+                else:
+                    break
 
+            events_list.append(event(name, start, end))
         
     return events_list
 
